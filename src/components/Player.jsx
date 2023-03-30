2270154a84050aff49ebf5d611b6f52a1febf269
Weil Essaisi <weil-97@hotmail.com>
2023-03-30 15:42:30 +0200

    added playlist cards

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 import { spotifyApi } from "@/pages/_app";
 import PlayerControls from "./PlayerControls";
 import PlayerVolume from "./PlayerVolume";
+import PlayerOverlay from "./PlayerOverlay";
 
 export default function Player() {
     const [device, setDevice] = useState(null);
@@ -9,6 +10,8 @@ export default function Player() {
     const [track, setTrack] = useState(null);
     const [isPaused, setIsPaused] = useState(false);
     const [position, setPosition] = useState(null);
+    const [playerOverlayIsOpen, setPlayerOverlayIsOpen] = useState(false);
+    const [isActive, setIsActive] = useState(false);
 
     useEffect(() => {
         const token = sessionStorage.getItem("spotify-key");
@@ -42,22 +45,19 @@ export default function Player() {
                 setTrack(state.track_window.current_track);
                 setIsPaused(state.paused);
                 setPosition(state.position);
+
+                player.getCurrentState().then((state) => {
+                    if (!state) {
+                        setIsActive(false);
+                    } else {
+                        setIsActive(true);
+                    }
+                });
             });
 
             player.connect();
         };
     }, []);
-
-    useEffect(() => {
-        async function getPlayback() {
-            if (device) {
-                await spotifyApi.transferMyPlayback([device], true);
-            }
-            await spotifyApi.getMyDevices();
-        }
-
-        getPlayback();
-    }, [device]);
 
     useEffect(() => {
         if (!localPlayer) return;
@@ -69,34 +69,49 @@ export default function Player() {
         };
     }, [localPlayer]);
 
-    if (!localPlayer || !track) return <div>no player, please connect.</div>;
+    if (!isActive || !localPlayer || !track)
+        return <div>No active session</div>;
 
     return (
-        <div className="flex items-center p-4">
-            <div className="flex flex-1 items-center">
-                <img
-                    src={track.album.images[0].url}
-                    alt=""
-                    className="mr-2 h-12 w-12 flex-shrink-0"
-                />
-                <div>
-                    <h4 className="">{track.name}</h4>
-                    <p className="text-xs text-text-dimmed">
-                        {track.artists[0].name}
-                    </p>
+        <div>
+            <div
+                className="flex items-center p-4"
+                onClick={() => {
+                    setPlayerOverlayIsOpen(!playerOverlayIsOpen);
+                }}
+            >
+                <div className="flex flex-1 items-center">
+                    <img
+                        src={track.album.images[0].url}
+                        alt=""
+                        className="mr-2 h-12 w-12 flex-shrink-0"
+                    />
+                    <div>
+                        <h4 className="">{track.name}</h4>
+                        <p className="text-xs text-text-dimmed">
+                            {track.artists[0].name}
+                        </p>
+                    </div>
+                </div>
+                <div className="flex-1 text-center max-md:hidden">
+                    <PlayerControls
+                        player={localPlayer}
+                        isPaused={isPaused}
+                        position={position}
+                        track={track}
+                    />
+                </div>
+                <div className="flex flex-1 justify-end max-md:hidden">
+                    <PlayerVolume player={localPlayer} />
                 </div>
             </div>
-            <div className="flex-1 text-center max-md:hidden ">
-                <PlayerControls
-                    player={localPlayer}
-                    isPaused={isPaused}
-                    position={position}
-                    track={track}
-                />
-            </div>
-            <div className="flex flex-1 justify-end max-md:hidden">
-                <PlayerVolume player={localPlayer} />
-            </div>
+            <PlayerOverlay
+                setPlayerOverlayIsOpen={setPlayerOverlayIsOpen}
+                playerOverlayIsOpen={playerOverlayIsOpen}
+                track={track}
+                isPaused={isPaused}
+                position={position}
+            />
         </div>
     );
 }
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,9 +1,46 @@
 import Layout from "@/components/Layout";
+import { useQuery } from "@tanstack/react-query";
+import Link from "next/link";
+import { spotifyApi } from "./_app";
 
 export default function index() {
+    const {
+        data: playlists,
+        isLoading,
+        isError,
+    } = useQuery({
+        queryKey: ["playlists"],
+        queryFn: async () => (await spotifyApi.getUserPlaylists()).body.items,
+    });
+    if (isLoading) return <p>loading...</p>;
+
+    if (isError) return <p>loading...</p>;
     return (
         <Layout>
-            <div>yaooo</div>
+            <div className="p-10">
+                <h1 className=" mb-5 text-4xl font-bold">
+                    Welcome to my spotify Clone
+                </h1>
+
+                <div className="grid grid-cols-2 gap-6 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5">
+                    {playlists.map((playlist) => (
+                        <Link
+                            href={`/playlists/${playlist.id}`}
+                            key={playlist.id}
+                            className="rounded-md bg-bg p-4"
+                        >
+                            <img
+                                src={playlist.images[0]?.url}
+                                alt=""
+                                className="aspect-square"
+                            />
+                            <p className="mt-4 truncate text-sm">
+                                {playlist.name}
+                            </p>
+                        </Link>
+                    ))}
+                </div>
+            </div>
         </Layout>
     );
 }
